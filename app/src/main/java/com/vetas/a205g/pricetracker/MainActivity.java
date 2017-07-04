dc385a91e460a899b5336c94a780d58a57b02b42
Huy Tran <huy.tran.tx@gmail.com>
2017-07-04 14:51:17 -0500

    Initial commit

---
+++ app/src/main/java/com/vetas/a205g/pricetracker/MainActivity.java
@@ -0,0 +1,13 @@
+package com.vetas.a205g.pricetracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
